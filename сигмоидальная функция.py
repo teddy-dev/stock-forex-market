41f9d07267734272d0406f1252a1f03592fde2b9
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2018-12-15 23:06:31 +0300

    Update сигмоидальная функция.py

--- сигмоидальная функция.py
+++ сигмоидальная функция.py
@@ -1,10 +1,15 @@
-import matplotlib.pylab as plt
-import numpy as np
-x = np.arange(-8, 8, 0.1)
-f = 1 / (1 + np.exp(-x))
-plt.plot(x, f)
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.show()
+def sigmofunc():
+  '''Через фун-ию работает ощутимо быстрее'''
+    import matplotlib.pylab as plt
+    import numpy as np
+    x = np.arange(-8, 8, 0.1)
+    f = 1 / (1 + np.exp(-x))
+    plt.plot(x, f)
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.show() 
+    
+if __name__ == '__main__':
+    sigmofunc()
 
 input('press enter..')
